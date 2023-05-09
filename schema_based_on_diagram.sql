1c18c691b0466073d641705743a0802bd18b90d7
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-09 20:06:52 +0300

    Add index on all foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -39,3 +39,10 @@ create table medical_histories_treatments(
 id int generated always as identity primary key,
 medical_history_id int references medical_histories(id),
 treatment_id int references treatments(id));
+
+create index patient_id_idx on medical_histories(patient_id);
+create index medical_history_id_idx on invoices(medical_history_id);
+create index invoice_id_idx on invoice_items(invoice_id);
+create index treatment_id_idx on invoice_items(treatment_id);
+create index medical_history_treatments_id_idx on medical_histories_treatments(medical_history_id);
+create index medical_histor_treatments_two_id_idx on medical_histories_treatments(treatment_id);
